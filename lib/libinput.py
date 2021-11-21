5e3c991901c964ca9e7e66ef91676ca20007bfa1
reversee-dev <rare7989@gmail.com>
2021-11-21 15:47:55 +0000

    libinput: implement Getch

--- lib/libinput.py
+++ lib/libinput.py
@@ -7,7 +7,7 @@ global userinput, cpuinput
 def getUserInput():
     userinput = 3
     while (userinput) == 3:
-        userinput = formatInput(input())
+        userinput = formatInput(getch())
     return(userinput)
 
 def formatInput(input):
@@ -17,5 +17,43 @@ def formatInput(input):
     elif (z.startswith("w") or z.startswith("W")): return 1
     elif (z.startswith("e") or z.startswith("E")): return 2
     elif (z.startswith("r") or z.startswith("R")): return 4
+    elif (z.startswith("X") or z.startswith("Z")): exit()
     else: return(3)
 
+# got code from https://code.activestate.com/recipes/134892/
+class _Getch:
+    def __init__(self):
+        try:
+            self.impl = _GetchWindows()
+        except ImportError:
+            self.impl = _GetchUnix()
+
+    def __call__(self): return self.impl()
+
+
+class _GetchUnix:
+    def __init__(self):
+        import tty, sys
+
+    def __call__(self):
+        import sys, tty, termios
+        fd = sys.stdin.fileno()
+        old_settings = termios.tcgetattr(fd)
+        try:
+            tty.setraw(sys.stdin.fileno())
+            ch = sys.stdin.read(1)
+        finally:
+            termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
+        return ch
+
+
+class _GetchWindows:
+    def __init__(self):
+        import msvcrt
+
+    def __call__(self):
+        import msvcrt
+        return msvcrt.getch()
+
+
+getch = _Getch()
