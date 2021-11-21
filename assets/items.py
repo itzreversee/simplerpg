b9f159a10c46e9b87b08637f6fa60947d7b7277c
reversee-dev <rare7989@gmail.com>
2021-11-21 15:48:18 +0000

    items: preparation

---
+++ assets/items.py
@@ -0,0 +1,10 @@
+class WoodenShield():
+    inGameName = 'Wooden Shield'
+    hardid = 'WoodenShield'
+    cost = 10
+    mana = 30
+    guard = 0.20
+    rarity = 'common'
+    level = 1
+
+#please dont use
