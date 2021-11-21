7a5490e0d22b7ae4a6f29db3e334e02a068df785
reversee-dev <rare7989@gmail.com>
2021-11-21 12:29:51 +0000

    libplayer --> libentity

---
+++ lib/libentity.py
@@ -0,0 +1,35 @@
+from lib.libmagic import * 
+
+class player():
+    name = "Maciek"
+
+    hp = 100
+    maxhp = 100
+    hpregen = 1
+
+    mana = 225
+    maxmana = 225
+    manaregen = 8
+
+    inventory = [FireSpell, CurseSpell, HealSpell]
+    isEnemy = False
+    curseLeft = 0
+    curseId = 6
+
+class silvermonster():
+    name = "Silver Monster"
+    id = 1
+
+    hp = 80
+    maxhp = 85
+    hpregen = 2
+
+    mana = 150
+    maxmana = 250
+    manaregen = 20
+
+    inventory = [IceSpell, AirSpell, HealSpell]
+    isEnemy = True
+    curseLeft = 0
+    curseId = 6
+
