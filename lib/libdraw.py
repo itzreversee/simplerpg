c726f34f004e0d08214d210173593d3d7f7f381d
reversee-dev <rare7989@gmail.com>
2021-11-21 21:19:57 +0000

    libdraw: drawing village and shop (broken)

--- lib/libdraw.py
+++ lib/libdraw.py
@@ -1,9 +1,44 @@
 
 from lib.libmagic import *
+from assets.items import *
 
-from lib.randomthings import notimplemented
+from lib.randomthings import notImplemented
 
-def drawBasicMenu(player, enemy, rounds): 
+def drawBasicWorldMenu(p):
+    print('\n')
+    print("| " + p.name + " |")
+    bar(p.exp, p.nextlevel, ' EXP  |', '| LEVEL - ' + str(p.level) + ', EXP until next LEVEL '+ str(p.nextlevel) + ', GOLD - ' + str(p.gold)); print("")
+    bar(p.hp, p.maxhp, ' HP   |', '| REGEN - ' + str(p.hpregen) ); print("")
+    bar(p.mana, p.maxmana, ' MANA |', '| REGEN - ' + str(p.manaregen) ); print("")
+
+    print("\nSpells:")
+    for i in range(len(p.inventory)):
+        print(" "+eval(p.inventory[i].hardid).name + ', cost: ' + str(eval(p.inventory[i].hardid).cost) + ' mana')
+   
+    print("\nItems:")
+    for i in range(len(p.items)):
+        print(" "+eval(p.items[i].hardid).inGameName + ', level: ' + str(eval(p.items[i].hardid).level))
+   
+
+def drawWorldMenu():
+    print("\nWhat to do?")
+    print(" q - shop ")
+    print(" w - sleep")
+    print(" e - exit village")
+    
+    return('village') # current menu
+
+def drawShopMenu(p, s):
+    print("\nWhat to buy? ")
+    for i in range(len(p.items)):
+       if (i) == 0: pkey = 'q - '
+       if (i) == 1: pkey = 'w - '
+       if (i) == 2: pkey = 'e - '
+       print(" "+ pkey + eval(p.items[i].hardid).inGameName + ', level: ' + str(eval(p.items[i].hardid).level))
+    
+    return('shop') # current menu
+
+def drawBasicBattleMenu(player, enemy, rounds): 
     print('\n')
     print("| " + player.name + " | vs | " + enemy.name + " (" + str(enemy.hp) + " HP) | ROUND " + str(rounds))
     bar(player.hp, player.maxhp, ' HP   |', '| REGEN - ' + str(player.hpregen) ); print("")
@@ -16,8 +51,7 @@ def drawBattleMenu():
     print(" e - skip round")
     
     return('battle') # current menu
-
-        
+  
 
 def drawSpellMenu(player):
     print("\nYour Spells: ")
@@ -41,11 +75,3 @@ def bar(iteration, total, prefix = ' |', suffix = '| ', ):
     filledLength = int(length * iteration // total)
     bar = fill * filledLength + '-' * (length - filledLength)
     print(f'\r{prefix} |{bar}| {percent}% ({iteration}) {suffix}', end = printEnd)
-
-def drawWorld(map): # DO NOT USE
-    lines = map.readlines()
-    for lines in lines:
-        print(lines)
-
-def drawWorldMenu(player):
-    print('a')
