29c0c814468f9cfa29872acdd4a11cddc79d2ad6
reversee <rare7989@gmail.com>
2021-12-14 19:44:02 +0100

    worldcore: add shops

--- lib/worldcore.py
+++ lib/worldcore.py
@@ -6,56 +6,71 @@ from assets.items import *
 
 from lib.libsave import *
 
-import time
+import time, random
 
-def world(save):
+def world(save, s):
+    global seed;
+    global ssave;
+    ssave = save;
+    seed = s
+    p = smanager.load(save)
     while True:
-        p = smanager.load(save)
-        smanager.save(save, checkStats(p)); p = smanager.load(save)
+        reload(save, p)
         if (p).location == 0: smallVillage(p);
+def reload(save, p):
+    smanager.save(save, checkStats(p)); p = smanager.load(save)
 
 def smallVillage(p):
     cmenu = 'village'
     clearConsole()
-    drawBasicWorldMenu(p)
+    drawBasicWorldMenu(p, 'Small Village')
     if (cmenu) == 'village':
         drawWorldMenu()
+        # for i in range(len(p.items)): print(p.items[i]) # debug
         (menuinput) = getUserInput()
-        if (menuinput) == 0: shop(p, getShopStock())
-        if (menuinput) == 1: notImplemented('sleep(p)')
+        if (menuinput) == 0: shop(p)
+        if (menuinput) == 1: notImplemented('sleep(p)'); global seed; seed += 200
         if (menuinput) == 2: print("you have exited the village."); time.sleep(2); exit()
     cmenu = 'village';  
-
-def updateStats(p, save):
-    a = checkStats(p)
-    smanager.save(save, a)
 
 def shop(p):
     inShop = True
     cmenu = 'shop'
     while inShop:
         clearConsole()
-        if (p.location != 'village'): inVillage = False; break
+        stock = getShopStock(3)
 
-        updateStats(p)
-
-        drawBasicWorldMenu(p)
+        drawBasicWorldMenu(p, 'SHOP')
         if (cmenu) == 'shop':
-                drawShopMenu(p, shopObject)
+                drawShopMenu(p, stock)
                 (menuinput) = getUserInput()
-                if (menuinput) == 0: shop(p)
-                if (menuinput) == 1: notImplemented('sleep(p)')
-                if (menuinput) == 2: print("you have exited the village."); exit()
+                if (menuinput) == 0: buy(p, stock[0])
+                if (menuinput) == 1: buy(p, stock[1])
+                if (menuinput) == 2: buy(p, stock[2])
                 if (menuinput) == 4: inShop = False; cmenu = 'village'; break;
         cmenu = 'shop';  
-        break;
-    
-class shopObject():
-    
-    itempool = [manaBag, healthNecklace]
-    print(len(itempool))
 
-    def getStock():
-        for i in range(3):
-            #return(shopObject.itempool[random.randint(0, shopObject.itempool.count-1)])
-            print('a')
+def buy(p, item):
+    print("\nYou are about to buy: "+item.inGameName)
+    print("\n Price: "+str(item.cost))
+    itemstats = getItemInfo(item); printItemInfo(itemstats)
+    (menuinput) = getUserInput()
+    if (menuinput) == 0:
+        if len(p.items) >= p.maxitems: print("You can't buy more than " + str(p.maxitems) + " items"); time.sleep(3); return;
+        if (item.cost > p.gold): print("You don't have enough gold"); time.sleep(3); return;
+        p.gold -= item.cost; p.items.append(item);
+        print("You bought " + item.inGameName + "!"); reload(ssave, p);
+        print("You bought " + item.inGameName + "!"); reload(ssave, p);
+        
+
+def getShopStock(count):
+    global seed;
+    
+    tseed = seed
+    itempool = [manaBag, healthNecklace]
+    items = []
+    for _ in range(count):
+        tseed *= 16
+        random.seed(tseed); 
+        items.append(itempool[random.randint(0,len(itempool) -1)])
+    return(items)
--- villagetest.py
+++ villagetest.py
@@ -9,12 +9,12 @@ import time
 
 clearConsole()
 
-maciek = basePlayer('Maciek', 0, 100, 100, 100, 1, 1, 225, 2255, 8, 200, 3, 3, [FireSpell, CurseSpell, HealSpell], [manaBag, manaBag, healthNecklace], 250, 120, 1, 100, False, 0, 6)
+maciek = basePlayer('Maciek', 0, 100, 100, 100, 1, 1, 225, 2255, 8, 200, 3, 3, [FireSpell, CurseSpell, HealSpell], [manaBag], 250, 0, 1, 100, False, 0, 6)
 print("simpleRPG. version: "+ game.version +' '+game.isStable() +" (Village Test)")
 time.sleep(1.5)
 
 def game():
     smanager.save("s0.pkl", maciek)
-    world("s0.pkl")
+    world("s0.pkl", random.randint(0,99999999))
 
 game()
