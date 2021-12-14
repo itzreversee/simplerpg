136c09b29418325cf015583e8d21dbbe14b6b85a
reversee <rare7989@gmail.com>
2021-12-14 21:06:13 +0100

    villagetest: change default stats

--- villagetest.py
+++ villagetest.py
@@ -9,7 +9,7 @@ import time
 
 clearConsole()
 
-maciek = basePlayer('Maciek', 0, 100, 100, 100, 1, 1, 225, 2255, 8, 200, 3, 3, [FireSpell, CurseSpell, HealSpell], [manaBag], 250, 0, 1, 100, False, 0, 6)
+maciek = basePlayer('Maciek', 0, 100, 100, 100, 1, 1, 200, 200, 8, 200, 3, 3, [FireSpell, CurseSpell, HealSpell], [], 250, 0, 1, 100, False, 0, 6)
 print("simpleRPG. version: "+ game.version +' '+game.isStable() +" (Village Test)")
 time.sleep(1.5)
 
