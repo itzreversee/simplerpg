6cbf02b4d081041e9bd2613809aac368f732ce47
reversee <46657288+reversee-dev@users.noreply.github.com>
2021-12-14 20:49:20 +0000

    battletest: recode for v2a

--- battletest.py
+++ battletest.py
@@ -1,18 +1,20 @@
 
+from lib.libsave import *
 from lib.libbattle import *
-from lib.libentity import *
+from lib.libentity import silvermonster
 from lib.randomthings import *
 import time
 
 clearConsole()
 
-
-print("simpleRPG. version: "+ game.version +' '+game.isStable() +" (Battle Test)")
-
+maciek = basePlayer('Maciek', 0, 100, 100, 100, 1, 1, 200, 200, 8, 200, 3, 3, [FireSpell, CurseSpell, HealSpell], [], 250, 0, 1, 100, False, 0, 6)
+print("simpleRPG. version: "+ game.version +' '+game.isStable() +" (runtime : Battle Test)")
 time.sleep(1.5)
 
 def game():
-    isBattleGoing = True
+    smanager.save("s_battletest.pkl", maciek)
+    player = smanager.load("s_battletest.pkl")
     battle(player, silvermonster)
+    print(player.hp)
 
 game()
