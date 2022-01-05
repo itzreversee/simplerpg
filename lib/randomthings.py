24855e076a701e3cb38a5893e1daa9b9d53c9477
reversee-dev <rare7989@gmail.com>
2022-01-05 20:17:04 +0100

    randomthings: add whatOS()

--- lib/randomthings.py
+++ lib/randomthings.py
@@ -8,12 +8,21 @@ def notImplemented(f = ''):
 
     return()
 
+def install(pkg):
+    import sys, subprocess
+    print(f" Installing - {pkg}")
+    subprocess.check_call([sys.executable, "-m", "pip", "install", pkg]); time.sleep(0.1);
 
 def clearConsole():
     command = 'clear'
     if os.name in ('nt', 'dos'): 
         command = 'cls'
     os.system(command)
+
+def whatOS():
+    if os.name in ('nt', 'dos'):
+        return('win')
+    else: return('unix')
 
 class game():
     version = '2.2a'
